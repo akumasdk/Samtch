c01503b06e54563accbfb12a1dbba7da06980dfd
akumasdk <magumboss1@gmail.com>
2026-09-01 15:04:45 -0600

    Bump version to 1.1.4 and update changelog with new features and enhancements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.magumboi.samtch"
         minSdk = 26
         targetSdk = 37
-        versionCode = 29
-        versionName = "1.1.3"
+        versionCode = 30
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
